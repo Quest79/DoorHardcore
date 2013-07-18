fbefa13e4480c0ed1acbbf7f3dc119001c942198
Paul Duncan <paul.shauna@gmail.com>
2013-07-17 23:18:49 -0700

    I have no idea what I'm doing.

--- pad/mods/hardoor/HardcoreDoor.java
+++ pad/mods/hardoor/HardcoreDoor.java
@@ -1,7 +1,9 @@
 package pad.mods.hardoor;
 
-import pad.mods.hardoor.items.ItemTutorial;
+import pad.mods.hardoor.items.Items16;
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Init;
@@ -11,6 +13,7 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
 @Mod(modid = HardcoreDoor.modID, name = "HardDoor", version = "1.0")
@@ -21,19 +24,33 @@ public class HardcoreDoor {
 	public static HardcoreDoor instance;
 	public static final String modID = "HardDoor";
 	
-	/* New Items and block secion */
-	public final static Item itemTutorial = new ItemTutorial(4000).setUnlocalizedName("itemtutorial");
-	public final static Item itemTutorial2 = new ItemTutorial(4001).setUnlocalizedName("itemtutorial2");
+	/* New Items section */
+	public final static Item hinge = new Items16(4000).setUnlocalizedName("Hinge2");
+	public final static Item woodpanel = new Items16(4001).setUnlocalizedName("woodpanel");
+	public final static Item iscrews = new Items16(4002).setUnlocalizedName("iscrews");
+	public final static Item hinge2 = new Items16(4003).setUnlocalizedName("Hinge3");
 	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event){
-		LanguageRegistry.addName(itemTutorial, "Tutorial Item");
-		LanguageRegistry.addName(itemTutorial2, "Tutorial Item 2");
+		
+		/* Language Registery */
+		LanguageRegistry.addName(hinge, "Hinge");
+		LanguageRegistry.addName(woodpanel, "Wooden Panel");
+		LanguageRegistry.addName(hinge2, "Hinge with Iron Screws");
+		LanguageRegistry.addName(iscrews, "Iron Screws");
+				
+		/* Recipes */
+		GameRegistry.addRecipe(new ItemStack(hinge, 1), new Object[]{"xxx", "x  ", "xxx", 'x', Item.ingotIron});
+		GameRegistry.addRecipe(new ItemStack(woodpanel, 1), new Object[]{"xx ", "xx ", "xx ", 'x', Block.wood});
+		GameRegistry.addRecipe(new ItemStack(iscrews, 1), new Object[]{" x ", "xxx", " x ", 'x', Item.ingotIron});
+		GameRegistry.addRecipe(new ItemStack(hinge2, 1), new Object[]{"x x", "xcx", "x x", 'x', iscrews, 'c', hinge});
+		GameRegistry.addRecipe(new ItemStack(Block.doorWood, 1), new Object[]{"xcc", "xcc", "xcc", 'x', hinge2, 'c', woodpanel});
+		
 	}
 	
 	@EventHandler
 	public void load(FMLLoadEvent event){
-		
+
 	}
 	
 	@EventHandler
